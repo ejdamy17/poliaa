944dd169105196df726343fc0749c82c8b69e06c
ejdamy17 <91655262+ejdamy17@users.noreply.github.com>
2022-03-24 09:24:13 +0100

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,39 +1,245 @@
-#include <iostream>
+#include < iostream >
+
+#include < conio.h >
+
 using namespace std;
+
+bool gameover;
+
+const int width = 20;
+
+const int height = 17;
+
+int x, y, fruitX, fruitY, score;
+
+int tailX[100], tailY[100]; //snake coordinates
+
+int nTail;
+
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN }; // Controls
+
+eDirecton dir;
+
+void Setup() {
+	gameover = false;
+
+	dir = STOP;
+
+	x = width / 2;
+
+	y = height / 2;
+
+	fruitX = rand() % width; //display fruit in a random place
+
+	fruitY = rand() % height; score = 0;
+
+}
+
+void Draw() {
+	system("cls");
+
+	for (int i = 0; i < width + 2; i++)
+
+		cout << "#";
+
+	cout << endl;
+
+	for (int i = 0; i < height; i++) {
+
+		for (int j = 0; j < width; j++) {
+
+			if (j == 0)
+
+				cout << "#"; //walls
+
+			if (i == y && j == x)
+
+				cout << "*"; // snake tale
+
+			else if (i == fruitY && j == fruitX)
+
+				cout << "%"; // change it to change the fruit
+
+			else {
+
+				bool print = false;
+
+				for (int k = 0; k < nTail; k++) {
+
+					if (tailX[k] == j && tailY[k] == i) {
+
+						cout << "*"; print = true;
+
+					}
+
+				}
+
+				if (!print) cout << " ";
+
+			}
+
+			if (j == width - 1)
+
+				cout << "#";
+
+		}
+
+		cout << endl;
+
+	}
+
+	for (int i = 0; i < width + 2; i++)
+
+		cout << "#";
+
+	cout << endl;
+
+	cout << "Score:" << score << endl;
+
+}
+
+void Input()
+{             
+
+	if (_kbhit()) {
+
+		switch (_getch()) {
+
+		case 'a':
+
+			dir = LEFT;
+
+			break;
+
+		case 'd':
+
+			dir = RIGHT;
+
+			break;
+
+		case 'w':
+
+			dir = UP;
+
+			break;
+
+		case 's':
+
+			dir = DOWN;
+
+			break;
+
+		case 'x':
+
+			gameover = true;
+
+			break;
+
+		}
+
+	}
+
+}
+
+void algorithm()
+{
+
+	int prevX = tailX[0];
+
+	int prevY = tailY[0];
+
+	int prev2X, prev2Y;
+
+	tailX[0] = x;
+
+	tailY[0] = y;
+
+	for (int i = 1; i < nTail; i++) {
+
+		prev2X = tailX[i];
+
+		prev2Y = tailY[i];
+
+		tailX[i] = prevX;
+
+		tailY[i] = prevY;
+
+		prevX = prev2X;
+
+		prevY = prev2Y;
+
+	}
+
+	switch (dir) {
+
+	case LEFT:
+
+		x--;
+
+		break;
+
+	case RIGHT:
+
+		x++;
+
+		break;
+
+	case UP:
+
+		y--;
+
+		break;
+
+	case DOWN:
+
+		y++;
+
+		break;
+
+	default:
+
+		break;
+
+	}
+
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+	for (int i = 0; i < nTail; i++)
+
+		if (tailX[i] == x && tailY[i] == y)
+			gameover = true;
+
+	if (x == fruitX && y == fruitY) {
+
+		score += 10;
+
+		fruitX = rand() % width;
+
+		fruitY = rand() % height;
+
+		nTail++;
+
+	}
+
+}
 
 int main()
 {
-    char pole[100];
-    int i; int cisla = 0, medzery = 0, specialneznaky = 0;
-    int samohlasky = 0, spoluhlasky = 0;
-    cout << "Napis hocico \n";
-    gets_s(pole);
-    for (i = 0; pole[i] != '\0'; i++) {
-        if (pole[i] == 'a' || pole[i] == 'e' || pole[i] == 'i' || pole[i] == 'o' || pole[i] == 'u' ||
-            pole[i] == 'A' || pole[i] == 'E' || pole[i] == 'I' || pole[i] == 'O' || pole[i] == 'U') {
-            samohlasky++;
-        }
-        else if (
-            (pole[i] >= 'a' && pole[i] <= 'z' || pole[i] >= 'A' && pole[i] <= 'Z')) {
-            spoluhlasky++;
-        }
-        else if (pole[i] >= '0' && pole[i] <= '9')
-        {
-            cisla++;
-        }
-        else if (pole[i] == ' ') {
-            medzery++;
-        }
-        else {
-            specialneznaky++;
-        }
-    }
-    system("CLS");
-    cout << "\nZadane pole: " << pole;
-    cout << "\nPocet samohlasok: " << samohlasky;
-    cout << "\nPocet spoluhlasok: " << spoluhlasky;
-    cout << "\nPocet cisel: " << cisla;
-    cout << "\nPocet medzier: " << medzery;
-    cout << "\nPocet specialnych znakov: " << specialneznaky;
-    return 0;
+
+	Setup();
+
+	while (!gameover) {
+
+		Draw();
+
+		Input();
+
+		algorithm();
+
+	}
+
+	return 0;
+
 }
